02f56e73e14a31213632fd4d24d178daf212de56
1f10230032 <s1f102300324@iniad.org>
2023-05-02 14:36:12 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,10 @@ import styles from './index.module.css';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.board} />
+      <div className={styles.board}>
+        <div className={styles.boards}>1</div>
+        <div className={styles.boards}>2</div>
+      </div>
     </div>
   );
 };
