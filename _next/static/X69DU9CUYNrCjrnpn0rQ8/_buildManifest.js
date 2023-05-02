8735ccc1180c19e066437d156dbae6b55b50cb1c
ys1729 <ys1729@users.noreply.github.com>
2023-05-02 05:39:45 +0000

    deploy: 3f8835a121886c85f376e8b263f73d89ee25eb1d

--- _next/static/X69DU9CUYNrCjrnpn0rQ8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/87c7ce9b2050d74e.css","static/chunks/pages/index-fe3f7af52eb0269a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/uPcsprUDasev2uk3IFewO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5d6690c2853d93e6.css","static/chunks/pages/index-fe3f7af52eb0269a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
