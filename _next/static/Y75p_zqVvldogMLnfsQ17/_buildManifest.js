e2e0fe15f37971de73006b37c6fcfa3263a4ea1d
ys1729 <ys1729@users.noreply.github.com>
2023-05-02 03:56:40 +0000

    deploy: e4dbae219b277debb802f6180e5ede0def9bfc35

---
+++ _next/static/Y75p_zqVvldogMLnfsQ17/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/33e87387dfaddfb2.css","static/chunks/pages/index-538ee8fa59893611.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
